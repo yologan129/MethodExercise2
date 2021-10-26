857d7dcad57971bac8ba0559e96cfadde01cc8cc
yologan129 <logan128128@gmail.com>
2021-10-26 10:08:39 -0500

    made a new exercise

---
+++ MethodsExercise2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodsExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Name: Michael
+                // Favorite Color: Blue
+                //Favorite Animal: Walrus
+                //Favorite Band: The Beatles
+                Console.WriteLine("Hello - What is your first name?");
+                var userName = Console.ReadLine();
+
+                Console.WriteLine($"Hello, {userName}. What is your favorite color.");
+                var color = Console.ReadLine();
+
+                Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+                var animal = Console.ReadLine();
+
+                Console.WriteLine("Great now what is your favorite{band}.");
+                var band = Console.ReadLine();
+
+                Console.WriteLine("Thanks, {userName}! here is your profile.");
+
+                Console.WriteLine($"Name {userName}");
+                Console.WriteLine($"Favorite color {color}");
+                Console.WriteLine($"Favorite Animal {animal}");
+                Console.WriteLine($"Favorite Band {band}");
+            }
+        }
+    }
+}
+
